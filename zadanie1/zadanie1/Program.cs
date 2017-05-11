d7bcb4510fed8dfa8edc882309c83ad26b4bf5a6
Patrycja Majchrowicz <p.majchrowicz@medicalgorithmics.com>
2017-05-11 10:21:12 +0200

    Aplikacja konsolowa wyświetlająca Hello World, komunikat o zamknięciu programu i zamyka program po kliknięciu dowolnego klawisza

---
+++ zadanie1/zadanie1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Aby zamknąć aplikację wciśnij dowolny klawisz.");
+            Console.ReadKey();
+        }
+    }
+}
