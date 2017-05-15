4b7bb88edd67a8854060bfd59cdb652c3e743237
Patrycja Majchrowicz <p.majchrowicz@medicalgorithmics.com>
2017-05-15 20:29:03 +0200

    zadanie2 - Wprowadzone poprawki do projektów, tak aby prawidłowo przebiegła kompilacja; wyznaczenie miejsc zerowych funkcji kwadratowej w Projekt4.

--- Projekt4/Projekt4/Program.cs
+++ Projekt4/Projekt4/Program.cs
@@ -25,14 +25,31 @@ namespace Zadanie4
             double x1 = 0;
             double x2 = 0;
 
-            // napisz obliczanie rozwiązań (miejsc zerowych) funkcji kwadratowej 
-            // jeśli nie pamiętasz jak to się liczy to tutaj jest ściąga
-            // http://matma.prv.pl/kwadratowa.php
-            // postaraj się napisac to samodzielnie a nie googlując implementację
-            // powodzenia :)
+            double delta;
+            delta = b*b - 4 * a * c;
+            if (delta > 0)
+            {
+                x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                Console.WriteLine("Dwa miejsca zerowe równe: ");
+                Console.WriteLine(x1);
+                Console.WriteLine(x2);
+            }
+            else if (delta == 0)
+            {
+                x1 = -b / (2 * a);
+                Console.WriteLine("Jedno miejsce zerowe równe: ");
+                Console.WriteLine(x1);
+            }
+            else // delta < 0
+            {
+                Console.WriteLine("Brak miejsc zerowych.");
+            }
             
-            Console.WriteLine(x1);
-            Console.WriteLine(x2);
+            
+            //Console.WriteLine(x1);
+            //Console.WriteLine(x2);
+            Console.ReadKey();
         }
     }
 }
