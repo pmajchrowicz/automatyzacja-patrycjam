931fcb206b07607d3fd892d61ed9e834c67c438a
Patrycja Majchrowicz <p.majchrowicz@medicalgorithmics.com>
2017-05-23 15:19:41 +0200

    cwiczenie1 - part3: aplikacja konsolowa + pierwsze wykonane testy

---
+++ ClassLibrary1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CwiczenieKonsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculator = new Calc(10,20);
+            Console.WriteLine("Suma wynosi: "+ calculator.Dodawanie());
+            /* var result = calculator.Dodawanie;
+             Console.WriteLine(result); */
+            Console.ReadKey();  // lub Console.ReadLine();
+            
+        }
+    }
+}
---
+++ ClassLibrary1/Ćwiczenie1.Tests/CalcTests.cs
@@ -0,0 +1,67 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Ćwiczenie1.Tests
+{
+    public class CalcTests
+    {
+        // jezeli_podam_5_i_10_wowczas_Dodawanie_zwraca_15
+
+        [Fact]   // dla .Framework i Xunit musi być [Fact] żeby traktowane było jako test jednostkowy
+        public void Dodawanie_zwraca_sume_dwoch_liczb()
+        {
+            // arrange
+            var calculator = new Calc(20, 15);
+
+            // act
+            var result = calculator.Dodawanie();
+
+            // assert
+            Assert.Equal(35, result);
+        }
+
+        [Fact]   
+        public void Odejmowanie_zwraca_roznice_dwoch_liczb()
+        {
+            // arrange
+            var calculator = new Calc(20, 15);
+
+            // act
+            var result = calculator.Odejmowanie();
+
+            // assert
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void Mnozenie_zwraca_iloczyn_dwoch_liczb()
+        {
+            // arrange
+            var calculator = new Calc(2, 5);
+
+            // act
+            var result = calculator.Mnozenie();
+
+            // assert
+            Assert.Equal(10, result);
+        }
+
+        [Fact]
+        public void Dzielenie_zwraca_iloraz_dwoch_liczb()
+        {
+            // arrange
+            var calculator = new Calc(20, 5);
+
+            // act
+            var result = calculator.Dzielenie();
+
+            // assert
+            Assert.Equal(4, result);
+        }
+    }
+}
