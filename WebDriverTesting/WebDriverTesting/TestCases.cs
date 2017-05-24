10963fcf72528a7a16d045875b3ce7e978b60d4e
Patrycja Majchrowicz <p.majchrowicz@medicalgorithmics.com>
2017-05-24 10:48:31 +0200

    selenium - part3: test sprawdzenia czy drugi tytuł (notka) na stronie brzmi Vivamus aliquam feugiat

--- WebDriverTesting/WebDriverTesting/TestCases.cs
+++ WebDriverTesting/WebDriverTesting/TestCases.cs
@@ -24,13 +24,24 @@ namespace WebDriverTesting
             _driver.Quit();
         } 
 
-        [Fact]
+        /*[Fact]
         public void First_note_should_be_Vivamus_aliguam_feugiat()
         {
             _driver.Navigate().GoToUrl("https://autotestdotnet.wordpress.com/");
             IWebElement firstPost = _driver.FindElementByClassName("post-title");
             string firstNoteTitle = firstPost.FindElement(By.TagName("a")).Text;
             Assert.Equal("Vivamus aliquam feugiat",firstNoteTitle);
+        } */
+
+        [Fact]
+        public void Second_note_should_be_Vivamus_aliguam_feugiat()
+        {
+            _driver.Navigate().GoToUrl("https://autotestdotnet.wordpress.com/");
+            var posts = _driver.FindElementsByClassName("post-title"); // szuka wszystkich class "post-title"
+            var secondPost = posts[1];  // drugi element ze znalezionych class "post-title"
+            string secondNoteTitle = secondPost.FindElement(By.TagName("a")).Text;
+            Assert.Equal("Vivamus aliquam feugiat", secondNoteTitle);
         }
+
     }
 }
