14be84a380f17faf3dbb83a41b508a1dd6b5143d
Patrycja Majchrowicz <p.majchrowicz@medicalgorithmics.com>
2017-05-25 09:31:59 +0200

    selenium_end - part11: Wersja końcowa testu sprawdzenia czy stworzona notka pojawia się na stronie

--- WebDriverTesting/WebDriverTesting/TestCases2.cs
+++ WebDriverTesting/WebDriverTesting/TestCases2.cs
@@ -2,10 +2,6 @@
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace WebDriverTesting
